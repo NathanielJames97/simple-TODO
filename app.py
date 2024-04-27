5c3b54d397a90185bfcf4640584ea5e2dc0b93e9
Spherox <65167045+NathanielJames97@users.noreply.github.com>
2024-04-27 19:25:19 +0100

    FIX - Todo's now save on refresh

--- app.py
+++ app.py
@@ -1,43 +1,55 @@
-from flask import Flask, render_template, request, redirect, url_for
+import os
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__, template_folder="templates")
+app.secret_key = os.environ.get('SECRET_KEY', 'default_secret_key')
 
-todos = [{"task": "Sample TODO", "done" : False}]
-
-
+def init_session():
+    if 'todos' not in session:
+        session['todos'] = [{"task": "Sample TODO", "done": False}]
 
 @app.route('/')
 def index():
+    init_session()
+    todos = session['todos']
     return render_template("index.html", todos=todos)
 
 @app.route("/add", methods=["POST"])
 def add():
+    init_session()
     todo = request.form['todo']
-    todos.append({"task": todo, "done": False})
+    session['todos'].append({"task": todo, "done": False})
+    session.modified = True  # Mark session as modified
     return redirect(url_for("index"))
-@app.route("/edit/<int:index>", methods={"GET", "POST"})
+
+
+@app.route("/edit/<int:index>", methods=["GET", "POST"])
 def edit(index):
+    init_session()
+    todos = session['todos']
     todo = todos[index]
-    if request.method == "POST" :
+    if request.method == "POST":
         todo['task'] = request.form["todo"]
+        session.modified = True  # Mark session as modified
         return redirect(url_for("index"))
     else:
         return render_template("edit.html", todo=todo, index=index)
-    
 
 @app.route("/check/<int:index>", methods=["POST"])
 def check(index):
-    if request.method == "POST":
-        todos[index]['done'] = not todos[index]['done']
-        return redirect(url_for("index"))
-
-
+    init_session()
+    todos = session['todos']
+    todos[index]['done'] = not todos[index]['done']
+    session.modified = True  # Mark session as modified
+    return redirect(url_for("index"))
 
 @app.route("/delete/<int:index>")
 def delete(index):
+    init_session()
+    todos = session['todos']
     del todos[index]
+    session.modified = True  # Mark session as modified
     return redirect(url_for("index"))
 
-
-if __name__== '__main__':
+if __name__ == '__main__':
     app.run(debug=True)
